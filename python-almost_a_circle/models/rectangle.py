439df66391b5c32c6a63f96174ffb2400d4557bd
kriverao98 <riveraocasiokoral29@gmail.com>
2023-02-16 23:51:33 -0400

    modified validation added eq, implemented area

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -14,44 +14,57 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """Returns the width of the rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Set the width of the rectangle."""
         self.check_type(value, "width")
         self.__width = value
             
     @property
     def height(self):
+        """Return the height of the rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Set the height of the rectangle."""
         self.check_type(value, "height")
         self.__height = value
             
     @property
     def x(self):
+        """Return the x-coordinate of the rectangle."""
         return self.__x
         
     @x.setter
     def x(self, value):
+        """Set the x-coordinate of the rectangle."""
         self.check_type(value, "x")
         self.__x = value
             
     @property
     def y(self):
+        """Return the y-coordinate of the rectangle."""
         return self.__y
         
     @y.setter
     def y(self, value):
+        """Set the y-coordinate of the rectangle."""
         self.check_type(value, "y")
         self.__y = value
             
-    def check_type(self, value, name):
-        if not isinstance(value, int):
+    def check_type(self, value, name, eq=True):
+            """Check if the value is of the correct type and raise an error if not."""
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
-        elif value <= 0:
-            raise ValueError("{} must be > 0".format(name))
-        elif value < 0:
+        if eq and value < 0:
             raise ValueError("{} must be >= 0".format(name))
+        elif not eq and value <= 0:
+            raise ValueError("{} must be > 0".format(name))
+        
+    def area(self):
+        """Returns area of rectangle"""
+        return self.width * self.height
