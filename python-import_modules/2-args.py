cf0ab4cf68d60f9f04e49d07e0fd947dd3dfc96e
kriverao98 <riveraocasiokoral29@gmail.com>
2024-01-26 00:50:22 -0400

    modified code to print correctly

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,7 +5,9 @@ if __name__ == "__main__":
     argv = sys.argv[1:]
     argc = len(argv)
 
-    print("{} arguments.".format(argc))
-    if argc > 0:
+    if argc == 0:
+        print("0 aguments.")
+    else:
+        print("{} arguments:".format(argc))
         for i, arg in enumerate(argv, start=1):
             print("{}: {}".format(i, arg))
