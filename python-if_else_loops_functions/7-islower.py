54db6d48e4791be08c329ab216cb7eb39a8b4063
kriverao98 <riveraocasiokoral29@gmail.com>
2024-01-26 18:19:04 -0400

    fixed task 7

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,6 @@
-#!/usr/bin/python3
+#!/usr/bin/env/python3
 def islower(c):
     if ord(c) >= 97 and ord(c) <= 122:
-        print(c, "is lower")
         return True
     else:
-        print(c, "is upper")
         return False
-    
