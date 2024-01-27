cd1bd26aa843920929e138c6362c3b5c5809a927
kriverao98 <riveraocasiokoral29@gmail.com>
2024-01-26 22:04:06 -0400

    fixed code added _name_...

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-a = 1
-b = 2
-
 from add_0 import add
 
-result = add(a, b)
-print("{} + {} = {}".format(a, b, result))
+a = 1
+b = 2
+if __name__ == "__main__":
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
