adbe8e1cb098e8b52ec6b5e1b680fd4d8d2ed5ab
kriverao98 <riveraocasiokoral29@gmail.com>
2024-01-31 11:14:36 -0400

    mashed 2 lines into one

--- python-more_data_structures/5-number_keys.py
+++ python-more_data_structures/5-number_keys.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    set_keys = len(a_dictionary)
-    set_keys = a_dictionary.keys
+    set_keys = len(a_dictionary.keys())
     print("Number of keys: {}".format(set_keys))
