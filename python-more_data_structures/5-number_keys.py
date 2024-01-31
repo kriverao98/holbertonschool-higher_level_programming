7929fb080139bd5ce67b80beead54b0a42965d84
kriverao98 <riveraocasiokoral29@gmail.com>
2024-01-31 11:11:45 -0400

    added variable to print line

--- python-more_data_structures/5-number_keys.py
+++ python-more_data_structures/5-number_keys.py
@@ -2,4 +2,4 @@
 def number_keys(a_dictionary):
     set_keys = len(a_dictionary)
     set_keys = a_dictionary.keys
-    print("Number of keys: {}".format())
+    print("Number of keys: {}".format(set_keys))
