62ec26d9a4f60e369123c5ff186d3202f266e4e7
kriverao98 <riveraocasiokoral29@gmail.com>
2024-02-01 12:21:46 -0400

    first commit, task1

---
+++ python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+class Square:
+    """Define a square"""
+
+    def __init__(self, size):
+        """The __init__ method initializes the size of the square"""
+        self.__size = size
