0e89962cf3c8b9936e771e446f756e36f6c0729f
kriverao98 <riveraocasiokoral29@gmail.com>
2024-02-01 13:04:05 -0400

    changed documentation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 class Square:
-    """Define a square"""
+    """This module creates a class Square"""
     def __init__(self, size):
-        """The __init__ method initializes the size of the square"""
         self.__size = size
