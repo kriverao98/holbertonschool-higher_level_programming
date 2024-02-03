50cc8a916ec341422c6318affb77fbf4edf75040
kriverao98 <riveraocasiokoral29@gmail.com>
2024-02-02 21:35:41 -0400

    trying to fix error

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""Module that contains a function that prints a text with 2 new lines after"""
+"""module prints 2 newline after char .,?,:"""
 
 
 def text_indentation(text):
-    """Function prints text with 2 newlines"""
-	if not isinstance(text, str):
-		raise TypeError("text must be a string")
+    """function prints text with 2 newlines"""
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
 
-	new_text = ""
-	for char in text:
-		new_text += char
-		if char in ['.', '?', ':']:
-			new_text += "\n\n"
+    for delimeter in "?:.":
+        words = (delimeter + "\n\n").join(
+                [index.strip(" ") for index in words.split(delimeter)])
 
-	print(new_text)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/5-text_indentation.txt")
