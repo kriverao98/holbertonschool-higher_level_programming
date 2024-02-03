96d9d465c4e4a532eb223c232f6224d01104711d
kriverao98 <riveraocasiokoral29@gmail.com>
2024-02-02 21:45:55 -0400

    fix

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""module prints 2 newline after char .,?,:"""
+"""text indent"""
 
 
 def text_indentation(text):
-    """function prints 2 newline after char .,?,:"""
+    """text indent"""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    new_text = ""
-    for char in text:
-        new_text += char
-        if char in ['.', '?', ':']:
-            new_text += "\n\n"
+    for delimeter in "?:.":
+        words = (delimeter + "\n\n").join(
+                [index.strip(" ") for index in words.split(delimeter)])
 
-    print(new_text.strip())
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/5-text_indentation.txt")
+    
