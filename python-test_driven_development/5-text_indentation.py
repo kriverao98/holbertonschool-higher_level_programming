c25fad1cec1e1e9839ba214c7d9711b5d78299a6
kriverao98 <riveraocasiokoral29@gmail.com>
2024-02-02 21:47:05 -0400

    maybe now

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -3,16 +3,16 @@
 
 
 def text_indentation(text):
-    """text indent"""
-    if not isinstance(text, str):
-        raise TypeError("text must be a string")
+	"""text indent"""
+	if not isinstance(text, str):
+		raise TypeError("text must be a string")
 
-    for delimeter in "?:.":
-        words = (delimeter + "\n\n").join(
-                [index.strip(" ") for index in words.split(delimeter)])
+	words = text
+	for delimeter in "?:.":
+		words = (delimeter + "\n\n").join(
+				[index.strip(" ") for index in words.split(delimeter)])
 
 
 if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/5-text_indentation.txt")
-    
+	import doctest
+	doctest.testfile("tests/5-text_indentation.txt")
