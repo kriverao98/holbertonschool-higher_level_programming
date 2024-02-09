38f434bcd4102cfb0c93973a4efd1c2b22812167
kriverao98 <riveraocasiokoral29@gmail.com>
2024-02-08 21:22:04 -0400

    added documentation

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+""" Rectangle class """
 
 
 class Rectangle:
+    """ Rectangle class """
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+""" Rectangle class """
 
 
 class Rectangle:
+    """ Rectangle class """
     number_of_instances = 0
     print_symbol = "#"
 
