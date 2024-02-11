2b01a6c900c67b852b88e9c313a850e4ad00452c
kriverao98 <riveraocasiokoral29@gmail.com>
2024-02-11 09:35:18 -0400

    fixed typo, added doc

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,12 @@
-#!/usr/bin/python
-
+#!/usr/bin/python3
+    """
+    Read and print the contents of a file.
+    """
 
 def read_file(filename=""):
-    with open("", "r") as file:
+    """
+    Read and print the contents of a file.
+    """
+    with open(filename, "r") as file:
         content = file.read()
         print(content)
