06b5746b1143156679f8daf7c5d58f37ec9e39b4
kriverao98 <riveraocasiokoral29@gmail.com>
2024-02-11 10:08:45 -0400

    fixed task 0, added utf-8

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python3
-"""Read and print the contents of a file."""
+#!/usr/bin/env python3
+"""Read and print the content of a file."""
 
 
 def read_file(filename=""):
-    """Read and print the contents of a file."""
-    with open(filename, "r") as file:
+    """Read and print the content of a file."""
+    with open(filename, "r", encoding="utf-8") as file:
         content = file.read()
         print(content)
