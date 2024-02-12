57a22af757cd6e7bd27d7ad547a17af4aeba559c
kriverao98 <riveraocasiokoral29@gmail.com>
2024-02-11 23:10:06 -0400

    fixed style

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,32 +1,33 @@
 #!/usr/bin/python3
-""" Rectangle class """
+"""Module that defines a rectangle."""
 
 
 class Rectangle:
-    """ Rectangle class """
-    
+    """Class that defines a rectangle."""
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-    
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
     def width(self, value):
-        try:
-            width += 1
-        except TypeError:
-            print("width must be an integer")
-            
-        try:
-            width < 0
-        except ValueError:
-            print("width must be >= 0")
-        
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
     def height(self, value):
-        try:
-            height += 1
-        except TypeError:
-            print("width must be an integer")
-            
-        try:
-            height < 0
-        except ValueError:
-            print("width must be >= 0")
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Rectangle class """
+"""This module defines a Rectangle class."""
 
 
 class Rectangle:
@@ -7,33 +7,33 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-        
+
     def width(self, value):
         try:
             width += 1
         except TypeError:
             print("width must be an integer")
-            
+
         try:
             width < 0
         except ValueError:
             print("width must be >= 0")
-        
+
     def height(self, value):
         try:
             height += 1
         except TypeError:
             print("width must be an integer")
-            
+
         try:
             height < 0
         except ValueError:
             print("width must be >= 0")
-            
+
     def area(self):
         area = self.width * self.height
         return area
-    
+
     def perimeter(self):
         if self.width == 0 or self.height == 0:
             return 0
