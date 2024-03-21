40101236c9808f8f81fa388d5caa4472c96ba39a
kriverao98 <riveraocasiokoral29@gmail.com>
2024-03-20 20:05:06 -0400

    Add Python scripts for filtering states by name

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,31 @@
+import MySQLdb
+import sys
+
+#!/usr/bin/python3
+
+
+if __name__ == "__main__":
+    # Get command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+
+    # Create a cursor object
+    cursor = db.cursor()
+
+    # Execute the query
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    # Fetch all the rows
+    rows = cursor.fetchall()
+
+    # Print the results
+    for row in rows:
+        print(row)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Get command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+
+    # Create a cursor object
+    cursor = db.cursor()
+
+    # Execute the SQL query
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
+    cursor.execute(query)
+
+    # Fetch all the rows
+    rows = cursor.fetchall()
+
+    # Display the results
+    for row in rows:
+        print(row)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
