d064c116aed003bc12f8461d947385ff08d8c5bf
kriverao98 <riveraocasiokoral29@gmail.com>
2024-03-22 09:57:40 -0400

    Fix SQL injection vulnerability by using parameterized query

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,22 +8,15 @@ if __name__ == '__main__':
 
     # Establish a connection to the database
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
+                         passwd=argv[2], db=argv[3], charset="utf8")
 
     # Create a cursor object to interact with the database
     cur = db.cursor()
-
-    # Execute the SQL query with a parameterized query to prevent SQL injections
-    cur.execute("SELECT * FROM states WHERE BINARY name = %s", [argv[4]])
-    
-
+    cur.execute("SELECT * FROM states WHERE BINARY name = %s", (argv[4]))
     # Fetch all the rows returned by the query
     rows = cur.fetchall()
-
-    # Print each row
+    # Print each row and closes process
     for row in rows:
         print(row)
-
-    # Clean up resources
     cur.close()
     db.close()
