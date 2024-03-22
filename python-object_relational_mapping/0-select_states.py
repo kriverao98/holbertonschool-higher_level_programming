7bee3d8a88131b95493cb1d240dc6feed1e5a9c6
kriverao98 <riveraocasiokoral29@gmail.com>
2024-03-22 00:40:46 -0400

    Refactor code to improve readability and add comments

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
-
 import MySQLdb
 from sys import argv
 
-if __name__ == '__main__':
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=argv[1], passwd=argv[2], db=argv[3])
+""" This script lists all states from the database hbtn_0e_0_usa """
 
+# The code should only be executed when run directly, not when imported
+if __name__ == '__main__':
+
+    # Establish a connection to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+
+    # Create a cursor object to interact with the database
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
 
+    # Fetch all the rows from the query result
     rows = cur.fetchall()
-    for i in rows:
-        print(i)
+    for row in rows:
+        print(row)
 
+    # Clean up resources
     cur.close()
     db.close()
