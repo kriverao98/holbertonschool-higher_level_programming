0cc029fc40341bb8efb03e81e419a6b95661853e
kriverao98 <riveraocasiokoral29@gmail.com>
2024-03-22 14:02:27 -0400

    Refactor code to use command line arguments and improve readability

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,21 +1,34 @@
 #!/usr/bin/python3
-from sys import argv
-from model_state import Base, State
-from sqlalchemy import (create_engine)
+import sys
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+"""
+Script that prints the State object with
+the name passed as argument from the database
+hbtn_0e_6_usa
+"""
+
 
 if __name__ == "__main__":
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'
-        .format(argv[1], argv[2],
-                argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    # Get command line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Create engine and session
+    engine = create_engine('mysql+mysqldb://{}:{}\
+                           @localhost:3306/{}'.format
+                           (mysql_username, mysql_password, database_name))
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).\
-        filter(State.name == argv[4]).order_by(State.id).all()
-    if states:
-        print("{}".format(states[0].id))
+
+    # Query the database for the state name
+    state = session.query(State).filter(State.name == state_name).first()
+
+    # Print the result
+    if state:
+        print(state.id)
     else:
         print("Not found")
-    session.close()
