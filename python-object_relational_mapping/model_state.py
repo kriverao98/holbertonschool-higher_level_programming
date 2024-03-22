eadd6824a815a7fef4205037323bbb9312af9458
kriverao98 <riveraocasiokoral29@gmail.com>
2024-03-22 10:35:49 -0400

    Refactor create_database function in model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-    """
-    Creates a database using SQLAlchemy and the provided arguments.
-    """
 from sys import argv
 from model_state import Base, State
+"""
+Creates a database using SQLAlchemy and the provided arguments.
+"""
 from sqlalchemy import (create_engine)
+
 
 def create_database():
     engine = create_engine(
@@ -12,6 +13,7 @@ def create_database():
         .format(argv[1], argv[2],
                 argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
+
 
 if __name__ == "__main__":
     create_database()
