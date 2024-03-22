9da89bde35dab17df5e93e2c535cea624c0355bb
kriverao98 <riveraocasiokoral29@gmail.com>
2024-03-22 10:30:29 -0400

    Refactor code to improve readability and maintainability

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
+"""Lists states"""
 
+import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
-    # Connect to the MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=argv[1], passwd=argv[2], db=argv[3],
-                         charset="utf8")
-    cursor = db.cursor()
-
-    cursor.execute("SELECT cities.id, cities.name, states.name "
-                   "FROM cities JOIN states ON cities.state_id = states.id "
-                   "ORDER BY cities.id ASC")
-
-    rows = cursor.fetchall()
-    for row in rows:
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities "
+                "JOIN states ON cities.state_id = states.id "
+                "ORDER BY cities.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
         print(row)
-    cursor.close()
-    db.close()
+    cur.close()
+    conn.close()
