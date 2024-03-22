6a59415607c8f9dcc04793fc62d35a3a7a2f23ad
kriverao98 <riveraocasiokoral29@gmail.com>
2024-03-22 09:42:53 -0400

    Add script to filter states by name with protection against MySQL injections

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+import MySQLdb
+from sys import argv
+
+"""
+Script that takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument
+while protecting against MySQL injections.
+"""
+
+# The code should only be executed when run directly, not when imported
+if __name__ == '__main__':
+
+    # Establish a connection to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+
+    # Create a cursor object to interact with the database
+    cur = db.cursor()
+
+    # Execute the SQL query with a parameterized query to prevent SQL injections
+    cur.execute("SELECT * FROM states WHERE BINARY name = %s", [argv[4]])
+
+    # Fetch all the rows returned by the query
+    rows = cur.fetchall()
+
+    # Print each row
+    for row in rows:
+        print(row)
+
+    # Clean up resources
+    cur.close()
+    db.close()
