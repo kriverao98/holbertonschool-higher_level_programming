aaac6c62b8070de46c3bc5b8515054ccd3b46628
kriverao98 <riveraocasiokoral29@gmail.com>
2024-03-22 10:25:28 -0400

    Refactor code to remove unnecessary variables and comments

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -4,30 +4,18 @@ import sys
 
 
 if __name__ == "__main__":
-    # Get the command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
     # Connect to the MySQL server
     db = MySQLdb.connect(host="localhost", port=3306,
-                         user=argv[1], passwd=argv[2], db=argv[3])
-
-    # Create a cursor object
+                         user=argv[1], passwd=argv[2], db=argv[3],
+                         charset="utf8")
     cursor = db.cursor()
 
-    # Execute the query
     cursor.execute("SELECT cities.id, cities.name, states.name "
                    "FROM cities JOIN states ON cities.state_id = states.id "
                    "ORDER BY cities.id ASC")
 
-    # Fetch all the rows
     rows = cursor.fetchall()
-
-    # Display the results
     for row in rows:
         print(row)
-
-    # Close the cursor and database connection
     cursor.close()
     db.close()
