b735092c1cf56761fa7e30db1889a8def41a1cfa
kriverao98 <riveraocasiokoral29@gmail.com>
2024-03-22 10:28:46 -0400

    Refactor code to use command line arguments and improve code readability

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,40 +1,19 @@
 #!/usr/bin/python3
-import sys
+"""Lists states"""
+
 import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
-    # Check if all arguments are provided
-    if len(sys.argv) != 5:
-        print("Usage: python 4-cities_by_state.py <mysql_username>\
-              <mysql_password> <database_name> <state_name>")
-        sys.exit(1)
-
-    # Get the command line arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-    state_name = sys.argv[4]
-
-    # Connect to the MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
-                         passwd=mysql_password, db=database_name)
-
-    # Create a cursor object to execute SQL queries
-    cursor = db.cursor()
-
-    # Execute the SQL query to retrieve cities of the given state
-    cursor.execute("SELECT cities.id, cities.name, states.name \
-                   FROM cities JOIN states ON cities.state_id = states.id \
-                   WHERE states.name = %s ORDER BY cities.id ASC",
-                   (state_name,))
-
-    # Fetch all the rows returned by the query
-    rows = cursor.fetchall()
-
-    # Display the results
-    for row in rows:
-        print(row)
-
-    # Close the cursor and database connection
-    cursor.close()
-    db.close()
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("""
+        SELECT cities.name FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+        """, (argv[4], ))
+    print(", ".join(map(lambda x: x[0], cur.fetchall())))
+    cur.close()
+    conn.close()
