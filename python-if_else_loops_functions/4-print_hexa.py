f27da2b681ced8a7e62b524a901fffbab4c96d0c
kriverao98 <riveraocasiokoral29@gmail.com>
2024-05-08 09:43:50 -0400

    chore: Refactor Python script to use f-strings for printing decimal numbers in hexadecimal format

---
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""
+Prints the decimal numbers from 0 to 98
+along with their hexadecimal representation.
+"""
+for i in range(0, 99):
+    print("{} = {}".format((i), (hex(i))))
