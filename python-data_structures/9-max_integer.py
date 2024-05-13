1e3cbcb24db82b46c28aebbd1a917fb3769480c9
kriverao98 <riveraocasiokoral29@gmail.com>
2024-05-13 17:04:17 -0400

    refactor: Fix indentation issue in max_integer function

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -5,12 +5,12 @@ def max_integer(my_list=[]):
     """
     Finds the maximum integer in a given list.
     """
-    if my_list == 0:
+    if my_list is None or len(my_list) == 0:
         return None
 
-    max = my_list[0]
+    max_val = my_list[0]
 
     for i in my_list:
-        if i > max:
-            max = i
-        return max
+        if i > max_val:
+            max_val = i
+    return max_val
