09f5473078d40ec0cd81c8faf3ae65db26fc2ffc
kriverao98 <riveraocasiokoral29@gmail.com>
2024-05-16 08:23:01 -0400

    added doc

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,7 +1,26 @@
 #!/usr/bin/python3
+"""This class returns area"""
+
 
 class Square:
-    def __init__(self,size=0):
+    """
+    This class represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square. Defaults to 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
         self.__size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
@@ -10,4 +29,10 @@ class Square:
         self.__size = size
 
     def area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
         return self.__size * self.__size
