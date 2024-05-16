ab1d3bfc603cf7b178d3941efe205638d5f0c77f
kriverao98 <riveraocasiokoral29@gmail.com>
2024-05-16 19:02:13 -0400

    refactor: Add position property to Square class

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -16,8 +16,9 @@ class Square:
         my_print: Prints a visual representation of the square.
     """
 
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         self.__size = size
+        self.position = position
 
     @property
     def size(self):
@@ -31,6 +32,18 @@ class Square:
             raise ValueError("size must be >= 0")
 
         self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def area(self):
         return self.__size ** 2
