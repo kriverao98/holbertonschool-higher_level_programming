6dfc75634abc21fea03bb77be3596059efd4b35e
kriverao98 <riveraocasiokoral29@gmail.com>
2024-05-20 12:49:31 -0400

    blank files, task 0

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def add_integer(a, b=98):
+    if type(a) != int:
+        raise TypeError("a must be an integer")
+    elif type(b) != int:
+        raise TypeError("b must be an integer")
+    else:
+        return a + b
