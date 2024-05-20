7425faacceb85c3c3917dadad48d09a7259991d0
kriverao98 <riveraocasiokoral29@gmail.com>
2024-05-20 18:44:49 -0400

    added doc

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -2,6 +2,19 @@
 
 
 def say_my_name(first_name, last_name=""):
+    """
+    Prints the name by concatenating the first_name and last_name.
+
+    Args:
+        first_name (str): The first name.
+        last_name (str, optional): The last name. Defaults to "".
+
+    Raises:
+        TypeError: If first_name or last_name is not a string.
+
+    Returns:
+        None
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
