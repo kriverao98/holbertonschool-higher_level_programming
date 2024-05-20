99f68536ce88fda703520d8bba97b7d0a2b5060d
kriverao98 <riveraocasiokoral29@gmail.com>
2024-05-20 13:16:15 -0400

    refactor: Update add_integer function to handle float inputs

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+"""Module to add two integers."""
 
 
 def add_integer(a, b=98):
-    if type(a) != int:
+    """Add two integers."""
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    elif type(b) != int:
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    else:
-        return a + b
+    return int(a) + int(b)
