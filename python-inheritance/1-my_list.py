727371ffed019d5b6d600a018ed040562be972b5
kriverao98 <riveraocasiokoral29@gmail.com>
2024-05-22 19:51:35 -0400

    Task 1, print sorted list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A class that inherits from list and prints a sorted list"""
+
+
+class MyList(list):
+    """This class inherits from list"""
+    def print_sorted(self):
+        """Prints a sorted list"""
+        print(sorted(self))
