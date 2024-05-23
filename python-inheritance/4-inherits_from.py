4b1eee3b72e9cf81f8f9e5383f4113d2b46ce119
kriverao98 <riveraocasiokoral29@gmail.com>
2024-05-22 23:40:56 -0400

    refactor: Update inherits_from function to exclude direct instances of the specified class

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""Check if an obj inherits from a specif class"""
-
 
 def inherits_from(obj, a_class):
     """
     Check if the object is an instance of
     a class that inherited (directly or indirectly) from the specified class.
     """
-    return isinstance(obj, a_class)
+    return isinstance(obj, a_class) and type(obj) is not a_class
