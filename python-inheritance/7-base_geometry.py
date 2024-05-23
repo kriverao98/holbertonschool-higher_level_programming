3d59e9b758685771efbca9e57b63481f0366086d
kriverao98 <riveraocasiokoral29@gmail.com>
2024-05-22 23:45:46 -0400

    refactor: Improve error messages in integer_validator method

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -3,13 +3,14 @@
 
 
 class BaseGeometry:
+    """BaseGeometry class"""
     def area(self):
         """Raises exception"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """Checks given value"""
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
