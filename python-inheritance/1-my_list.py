e9d424da79c66d8a1b4bfa74b623ab38ede178f6
kriverao98 <riveraocasiokoral29@gmail.com>
2024-05-22 23:38:07 -0400

    refactor: Fix TypeError and ValueError messages in integer_validator method

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,6 +4,9 @@
 
 class MyList(list):
     """This class inherits from list"""
+    def __init__(self, *args):
+        super().__init__(*args)
+
     def print_sorted(self):
         """Prints a sorted list"""
         print(sorted(self))
