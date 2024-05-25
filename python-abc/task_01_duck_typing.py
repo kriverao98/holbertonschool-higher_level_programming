1a52fc06f20485c92b5994666d85683f48cc4015
kriverao98 <riveraocasiokoral29@gmail.com>
2024-05-24 22:20:16 -0400

    refactor: Implement abstract Shape class and Circle/Rectangle subclasses

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,54 @@
+import math
+from abc import ABC, abstractmethod
+
+#!/usr/bin/python3
+
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+    
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+    
+    def area(self):
+        return math.pi * self.radius ** 2
+    
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    
+    def area(self):
+        return self.width * self.height
+    
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+
+def shape_info(shape):
+    try:
+        area = shape.area()
+        perimeter = shape.perimeter()
+        print(f"Area: {area}")
+        print(f"Perimeter: {perimeter}")
+    except AttributeError as e:
+        print(f"Error: {e}")
+
+
+circle = Circle(5)
+rectangle = Rectangle(4, 6)
+
+shape_info(circle)
+shape_info(rectangle)
