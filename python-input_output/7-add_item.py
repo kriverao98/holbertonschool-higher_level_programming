0599fe8db90fa41efc97e35467ab99940bcb3de7
kriverao98 <riveraocasiokoral29@gmail.com>
2024-05-28 19:13:49 -0400

    Task 7 done. Empty files 8-12

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+args = sys.argv[1:]
+filename = "add_item.json"
+
+try:
+    # Load existing data from file
+    data = load_from_json_file(filename)
+except FileNotFoundError:
+    # If file doesn't exist, initialize an empty list
+    data = []
+
+# Add arguments to the list
+data.extend(args)
+
+# Save the updated list to the file
+save_to_json_file(data, filename)
