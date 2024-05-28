e05353ea8d51637f9b31b1c21bdcbeeb34383544
kriverao98 <riveraocasiokoral29@gmail.com>
2024-05-28 19:32:05 -0400

    feat: Add to_json method to Student class for JSON serialization

---
+++ python-input_output/9-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""A class representing a student."""
+
+
+class Student:
+    """
+    Represents a student.
+
+    Attributes:
+        first_name (str): The first name of the student.
+        last_name (str): The last name of the student.
+        age (int): The age of the student.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Returns a dictionary representation of the student.
+        """
+        return self.__dict__
