cf2731ae533ffcef93220df47cc3eb12553d637a
kriverao98 <riveraocasiokoral29@gmail.com>
2024-05-28 17:00:58 -0400

    feat: Add append_write function to append text to a file

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Append string to end of txt file"""
+
+
+def append_write(filename="", text=""):
+    """
+    Appends a string to the end of a text file and returns the number of characters written.
+
+    Args:
+        filename (str): The name of the file to append to.
+        text (str): The string to append to the file.
+
+    Returns:
+        int: The number of characters written to the file.
+    """
+    with open(filename, 'a', encoding="utf-8") as file:
+        char_num = file.write(text)
+    return char_num
