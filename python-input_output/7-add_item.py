4abc3a75512f9c8bd07c047741500b9f0a179204
kriverao98 <riveraocasiokoral29@gmail.com>
2024-05-28 19:14:48 -0400

    chore: Add docstring to 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Saves an update list using function 5 and 6"""
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
