1ef7694c4fa30d44af3b170b2a296cc18f851d38
kriverao98 <riveraocasiokoral29@gmail.com>
2024-05-30 22:30:00 -0400

    feat: Update fetch_and_print_posts and fetch_and_save_posts functions

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,56 +1,29 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python3
 """This module fetches data from specified url"""
 import requests
+import json
 import csv
 
 
-def fetch_and_print_posts(url):
-    """
-    Fetches data from the specified URL and prints the posts.
-
-    Args:
-        url (str): The URL to fetch the data from.
-
-    Returns:
-        None
-    """
-    get_requests = requests.get(url)
-    print(f"Status Code: {response.get_requests}")
-    if response.get_requests == 200:
-        data = response.json()
-        print(json.dumps(data, indent=4))
-    else:
-        print("Failed to retrieve data")
-
-
-def fetch_and_save_posts(url):
-    """
-    Fetches data from the specified URL and saves the posts to a CSV file.
-
-    Args:
-        url (str): The URL to fetch the data from.
-
-    Returns:
-        None
-    """
-    response = requests.get(url)
+def fetch_and_print_posts():
+    response = requests.get('https://jsonplaceholder.typicode.com')
+    status_code = response.status_code
+    print(f"Status Code: {status_code}")
+    
+    if status_code == 200:
+        posts = response.json()
+        for post in posts:
+                print(post["title"])
+def fetch_and_save_posts():
+    response = requests.get('https://jsonplaceholder.typicode.com')
+    status_code = response.status_code
     if response.status_code == 200:
-        data = response.json()
-        posts = []
-        for post in data:
-            post_data = {
-                'id': post['id'],
-                'title': post['title'],
-                'body': post['body']
-            }
-            posts.append(post_data)
-            
-        with open('posts.csv', 'w', newline='') as csvfile:
-            fieldnames = ['id', 'title', 'body']
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        posts = response.json()
+        with open('posts.csv', mode='w') as file:
+            writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
             writer.writeheader()
-            writer.writerows(posts)
-            
-        print("Data saved to posts.csv")
+            for post in posts:
+                writer.writerow({'id': post['id'], 'title': post['title'], 'body': post['body']})
+        return True
     else:
-        print("Failed to retrieve data")
+        return False
