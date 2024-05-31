450833cf379e29234da0678d66f5e7bde02470f6
kriverao98 <riveraocasiokoral29@gmail.com>
2024-05-30 23:20:39 -0400

    feat: Add new user endpoint with error handling for missing username

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,46 +1,38 @@
-#!/usr/local/bin/python3
+#!/usr/bin/python3
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-# Dictionary to store user data
-users = {
-    "jane": {"name": "Jane", "age": 28, "city": "Los Angeles"}
-}
+users = {"jane": {"name": "Jane", "age": 28, "city": "Los Angeles"}}
 
-# Endpoint for root URL
-@app.route('/')
+@app.route("/")
 def home():
     return "Welcome to the Flask API!"
 
-# Endpoint to serve JSON data
-@app.route('/data')
+@app.route("/data")
 def get_data():
     return jsonify(list(users.keys()))
 
-# Endpoint for /status
-@app.route('/status')
+@app.route("/status")
 def get_status():
     return "OK"
 
-# Endpoint to get user details by username
-@app.route('/users/<username>')
+@app.route("/users/<username>")
 def get_user(username):
     if username in users:
         return jsonify(users[username])
     else:
         return "User not found", 404
 
-# Endpoint to add new user
-@app.route('/add_user', methods=['POST'])
+@app.route("/add_user", methods=["POST"])
 def add_user():
     data = request.get_json()
-    if 'username' in data:
-        username = data['username']
-        users[username] = data
-        return f"User {username} added successfully", 201
-    else:
-        return "Missing username in request data", 400
+    username = data.get("username")
+    name = data.get("name")
+    age = data.get("age")
+    city = data.get("city")
+    users[username] = {"name": name, "age": age, "city": city}
+    return jsonify(users[username])
 
 if __name__ == "__main__":
-    app.run
+    app.run()
