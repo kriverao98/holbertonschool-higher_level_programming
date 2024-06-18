c7db427b2e9ec548c01ed563b6b00b7cb7922e93
kriverao98 <riveraocasiokoral29@gmail.com>
2024-06-18 19:17:21 -0400

    task 9, output is similar

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- This script that lists all cities contained in the database
+SELECT cities.id, cities.name AS city_name, states.name AS state_name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
