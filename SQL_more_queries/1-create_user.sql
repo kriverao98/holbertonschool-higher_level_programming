4704e1f5d4a0493dbc37ce297bf2e68cf50b11b7
kriverao98 <riveraocasiokoral29@gmail.com>
2024-06-18 17:33:30 -0400

    fixed mysql syntax

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,9 +1,7 @@
 -- Create user with all privileges
-CREATE USER
-    IF NOT EXISTS 'user_0d_1'@'localhost'
+CREATE USER 'user_0d_1'@'localhost'
     IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
    ON *.*
-   TO 'user_0d_1'@'localhost'
-   IDENTIFIED BY 'user_0d_1_pwd';
+   TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
