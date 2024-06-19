74b1e80c600dcda1db396ed86061eeb88bfacb54
kriverao98 <riveraocasiokoral29@gmail.com>
2024-06-18 21:05:50 -0400

    fixed syntax on 14

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,6 @@
 --This script uses the database to lists all genres of a show
-SELECT tv_genres.name
-FROM tv_shows
-JOIN tv_genres ON tv_shows.genre_id = tv_genres.id
+SELECT name FROM tv_genres
+JOIN tv_shows ON id=tv_show_genres.genre_id
+JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
 WHERE tv_shows.title = 'Dexter'
-ORDER BY tv_genres.name ASC;
+ORDER BY name;
