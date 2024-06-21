59fb29eefbef48576e6b4a46f04742b6c13b97e6
kriverao98 <riveraocasiokoral29@gmail.com>
2024-06-21 09:52:29 -0400

    chore: Refactor code to print the State object by name from the database

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -4,19 +4,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-def print_state_by_name(mysql_username, mysql_password, database_name, state_name):
-    """
-    Prints the State object with the name passed as argument from the database.
+"""
+Script that prints the State object with
+the name passed as argument from the database
+hbtn_0e_6_usa
+"""
 
-    Args:
-        mysql_username (str): The MySQL username.
-        mysql_password (str): The MySQL password.
-        database_name (str): The name of the database.
-        state_name (str): The name of the state to search for.
+if __name__ == "__main__":
+    # Get command line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
 
-    Returns:
-        None
-    """
     # Create engine and session
     engine = create_engine('mysql+mysqldb://{}:{}\
                            @localhost:3306/{}'.format
@@ -32,13 +32,3 @@ def print_state_by_name(mysql_username, mysql_password, database_name, state_nam
         print(state.id)
     else:
         print("Not found")
-
-if __name__ == "__main__":
-    # Get command line arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-    state_name = sys.argv[4]
-
-    # Call the function
-    print_state_by_name(mysql_username, mysql_password, database_name, state_name)
