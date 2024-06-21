10897e8e77320cd85ea0bbdb6027c4f87a979d2a
kriverao98 <riveraocasiokoral29@gmail.com>
2024-06-20 22:12:39 -0400

    chore: Refactor code to select and print states starting with 'N' from the database

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+This script lists all the states with a name starting with N
+from the hbtn_0e_0_usa database.
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", port=3306, username=argv[1], password=argv[2], db_name=argv[3])
+
+    cursor = db.cursor()
+    cursor.execute=("SELECT * FROM states WHERE states LIKE 'N%' ORDER BY id ASC");
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
