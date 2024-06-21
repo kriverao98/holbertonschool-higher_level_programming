020af22bebb838b9409409108a71eb526b097c28
kriverao98 <riveraocasiokoral29@gmail.com>
2024-06-21 12:41:53 -0400

    refactor: Update script to get a state from the database by name

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -11,24 +11,20 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
-    # Get command line arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-    state_name = sys.argv[4]
+    """
+    Access to the database and get a state
+    from the database.
+    """
 
-    # Create engine and session
-    engine = create_engine('mysql+mysqldb://{}:{}\
-                           @localhost:3306/{}'.format
-                           (mysql_username, mysql_password, database_name))
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(db_uri)
     Session = sessionmaker(bind=engine)
+
     session = Session()
+    instance = session.query(State).filter(State.name == argv[4]).first()
 
-    # Query the database for the state name
-    state = session.query(State).filter(State.name == state_name).first()
-
-    # Print the result
-    if state:
-        print(state.id)
+    if instance is None:
+        print('Not found')
     else:
-        print("Not found")
+        print('{0}'.format(instance.id))
