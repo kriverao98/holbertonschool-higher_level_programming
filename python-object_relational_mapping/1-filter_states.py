36a5f84a83a61c9f674939baf3ef31d9d3a8743e
kriverao98 <riveraocasiokoral29@gmail.com>
2024-06-20 22:13:13 -0400

    Refactor code to fix SQL query in task 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306, username=argv[1], password=argv[2], db_name=argv[3])
 
     cursor = db.cursor()
-    cursor.execute=("SELECT * FROM states WHERE states LIKE 'N%' ORDER BY id ASC");
+    cursor.execute=("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC");
 
     rows = cursor.fetchall()
 
