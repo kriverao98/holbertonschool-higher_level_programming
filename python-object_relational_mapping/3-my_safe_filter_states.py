6220fe6b93b7b55a070afab40807eba0af07247d
kriverao98 <riveraocasiokoral29@gmail.com>
2024-06-20 23:44:53 -0400

    Refactor code to use LIKE BINARY for case-sensitive search in task 3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,13 +8,14 @@ if __name__ == '__main__':
 
     # Establish a connection to the database
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
+                         passwd=argv[2], db=argv[3])
 
     # Create a cursor object to interact with the database
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states\
+    with db.cursor() as cur:
+        cur.execute("SELECT * FROM states\
             WHERE name LIKE BINARY %(name)s\
             ORDER BY states.id ASC", {'name': argv[4]})
+
     # Fetch all the rows returned by the query
     rows = cur.fetchall()
     # Print each row and closes process
