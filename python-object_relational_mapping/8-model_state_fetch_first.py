0347b7afef301b77c61d873f10c13767f678503c
kriverao98 <riveraocasiokoral29@gmail.com>
2024-06-21 01:31:52 -0400

    chore: Fetch and print the first state from the database

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def fetch_first_state():
+    """
+    Fetches the first state from the database and prints its id and name.
+    If no state is found, it prints "Nothing".
+    """
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).order_by(State.id).first()
+    if state is None:
+        print("Nothing")
+    else:
+        print("{}: {}".format(state.id, state.name))
+    session.close()
