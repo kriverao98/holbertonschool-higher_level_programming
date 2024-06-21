da68d63401fb6dc0f7f0384b202bd03ec8ec34d8
kriverao98 <riveraocasiokoral29@gmail.com>
2024-06-21 12:26:08 -0400

    documented code

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+This script defines a State class and a Base class.
+"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -7,6 +10,13 @@ Base = declarative_base()
 
 
 class State(Base):
+    """
+    Represents a state in the database.
+
+    Attributes:
+        id (int): The unique identifier of the state.
+        name (str): The name of the state.
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
